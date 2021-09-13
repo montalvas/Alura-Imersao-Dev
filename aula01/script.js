1088632ccb2b38ca9f8de2677f40444b75f31a71
Montalvas <62609007+montalvas@users.noreply.github.com>
2021-09-13 19:11:42 -0300

    update

--- aula01/script.js
+++ aula01/script.js
@@ -1,2 +1,12 @@
 var nome = "Montalvas";
-console.log("Boas vindas " + nome) 
+var notaPrimeiroBimestre = 10
+var notaSegundoBimestre = 9
+var notaTerceiroBimestre = 7.5
+var notaQuartoBimestre = 8.8
+
+var notaFinal = (notaPrimeiroBimestre + notaSegundoBimestre + notaTerceiroBimestre + notaQuartoBimestre) / 4
+var notaFixada = notaFinal.toFixed(1)
+
+console.log("Boas vindas " + nome)
+console.log("Nota final: " + notaFixada)
+// var, strings, console.log, operacao, concatenacao
