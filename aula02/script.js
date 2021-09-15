6e051903d46357e0fc58c15589d7462d56220c9f
Montalvas <lucas_montalvani@hotmail.com>
2021-09-14 21:57:38 -0300

    aula02

--- aula02/script.js
+++ aula02/script.js
@@ -1,6 +1,12 @@
 function Converter() {
   //console.log('Clicou') a função se conecta com o atributado do input 'onclick'
-  var valorElemento =  parseFloat(document.getElementById('valor').value);
-  var valorEmDolar = valorElemento/5.25;
-  console.log(valorEmDolar);
+  var valorElemento =  parseFloat(document.getElementById('valor').value); //pego valor input
+  var cotacaoDolar = 5.25;
+  var valorEmReal = valorElemento * cotacaoDolar;
+
+  var elementoValorConvertido = document.getElementById('valorConvertido') //pego a div
+  var valorConvertido = 'O resultado em real é R$ ' + valorEmReal
+  
+  elementoValorConvertido.innerHTML = valorConvertido
+
 }
