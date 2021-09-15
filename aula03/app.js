40a0ee9c0c72b129a80ba7a1f3c9f22dbd1d2557
Montalvas <lucas_montalvani@hotmail.com>
2021-09-15 13:58:46 -0300

    fix

--- aula03/app.js
+++ aula03/app.js
@@ -4,12 +4,13 @@ var tentativas = 0;
 function Chutar() {
   tentativas++;
   var valorChute = parseInt(document.getElementById('valor').value);
+  var elementoResultado = document.getElementById('resultado')
   
   if (valorChute > 10 || valorChute < 0) {
-    alert('Chute inválido, entre com um número de 0 a 10.')
+    elementoResultado.innerHTML = 'Chute inválido, entre com um número de 0 a 10.'
   } else {
     if (valorChute == numeroSorteado) {
-      alert('Parabéns você acertou com ' + tentativas + ' tentativas!');
+      elementoResultado.innerHTML = 'Parabéns você acertou com ' + tentativas + ' tentativas!';
     } else {
       if (valorChute > numeroSorteado) {
         alert('Valor maior que número sorteado.')
