ad2597bb5e453c7dfe17b6a3183e43d60fc21688
Montalvas <lucas_montalvani@hotmail.com>
2021-09-16 14:04:38 -0300

    fix

--- aula04/aula04.js
+++ aula04/aula04.js
@@ -2,11 +2,14 @@ posters = ['https://images4.alphacoders.com/128/128706.png',
 'https://images.alphacoders.com/846/thumb-1920-84631.jpg',
 'https://images7.alphacoders.com/469/thumb-1920-469029.jpg']
 
+galeria = document.getElementById('galeria')
+
+
 for (poster in posters) {
-  document.write(`<img src="${posters[poster]}">`)
+  galeria.innerHTML += `<img src="${posters[poster]}">`
 }
 
 function Enviar() {
-  novaImagem = document.getElementById('arquivo').
-  posters.push(novaImagem)
+  linkImagem = document.getElementById('link').value
+  galeria.innerHTML += `<img src="${linkImagem}">`
 }
