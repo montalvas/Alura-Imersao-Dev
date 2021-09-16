191bea56b37e6f58d3de91c6c20b44c057268a41
montalvas <lucas_montalvani@hotmail.com>
2021-09-16 11:58:46 -0300

    aula04

---
+++ aula04/aula04.js
@@ -0,0 +1,12 @@
+posters = ['https://images4.alphacoders.com/128/128706.png',
+'https://images.alphacoders.com/846/thumb-1920-84631.jpg',
+'https://images7.alphacoders.com/469/thumb-1920-469029.jpg']
+
+for (poster in posters) {
+  document.write(`<img src="${posters[poster]}">`)
+}
+
+function Enviar() {
+  novaImagem = document.getElementById('arquivo').
+  posters.push(novaImagem)
+}
