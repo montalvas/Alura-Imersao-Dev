d03fd82c30531426786505ea59ef7d43d8e64b1b
Montalvas <lucas_montalvani@hotmail.com>
2021-09-17 20:02:53 -0300

    check extension fix

--- aula05/script.js
+++ aula05/script.js
@@ -7,30 +7,34 @@ function adicionarFilme() {
   var name = document.getElementById('nomeFilme').value
   var achouFilme = false
 
-  for (filme in listaFilmes) {
-    if (listaFilmes[filme].includes(name) || listaFilmes[filme].includes(link)) {
-      alert('Filme ou imagem já existe no catálogo')
-      achouFilme = true
+  if (link.endsWith('.jpg')) {
+    for (filme in listaFilmes) {
+      if (listaFilmes[filme].includes(name) || listaFilmes[filme].includes(link)) {
+        alert('Filme ou imagem já existe no catálogo')
+        achouFilme = true
+      }
     }
-  }
 
-  /*
-  Primeiro executa um loop para procurar se o nome da imagem ou o link já existe no array
-  Assim eu faço o controle das imagens pelo nome/link presente no array
-  Caso sim, ele exibe um aviso
-  Caso não, a variável de condição 'achouFilme' continua falsa
-  */
+    /*
+    Primeiro executa um loop para procurar se o nome da imagem ou o link já existe no array
+    Assim eu faço o controle das imagens pelo nome/link presente no array
+    Caso sim, ele exibe um aviso
+    Caso não, a variável de condição 'achouFilme' continua falsa
+    */
 
-  if (achouFilme == false) {
-    listaFilmes.push([name, link])
-    mostrarFilmes.innerHTML += `
-    <figure class="${idFilme}">
-	    <img src="${link}">
-	    <figcaption>${name}</figcaption>
-      <button onClick="deletarImagem(this)">Excluir</button>
-    </figure>
-    `
-    idFilme++
+    if (achouFilme == false) {
+      listaFilmes.push([name, link])
+      mostrarFilmes.innerHTML += `
+      <figure class="${idFilme}">
+        <img src="${link}">
+        <figcaption>${name}</figcaption>
+        <button onClick="deletarImagem(this)">Excluir</button>
+      </figure>
+      `
+      idFilme++
+    }
+  } else {
+    console.error("Endereço inválido")
   }
 
   /*
