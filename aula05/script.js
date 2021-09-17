673dcde84243716406a5501c8ed434745b7a5362
Montalvas <lucas_montalvani@hotmail.com>
2021-09-17 19:09:15 -0300

    fix

--- aula05/script.js
+++ aula05/script.js
@@ -1,4 +1,5 @@
 var listaFilmes = []
+var idFilme = 0
 var mostrarFilmes = document.getElementById('listaFilmes')
 
 function adicionarFilme() {
@@ -7,26 +8,45 @@ function adicionarFilme() {
   var achouFilme = false
 
   for (filme in listaFilmes) {
-    if (listaFilmes[filme].includes(name)) {
+    if (listaFilmes[filme].includes(name) || listaFilmes[filme].includes(link)) {
       alert('Filme ou imagem já existe no catálogo')
       achouFilme = true
     }
   }
 
+  /*
+  Primeiro executa um loop para procurar se o nome da imagem ou o link já existe no array
+  Assim eu faço o controle das imagens pelo nome/link presente no array
+  Caso sim, ele exibe um aviso
+  Caso não, a variável de condição 'achouFilme' continua falsa
+  */
+
   if (achouFilme == false) {
     listaFilmes.push([name, link])
     mostrarFilmes.innerHTML += `
-    <figure>
+    <figure class="${idFilme}">
 	    <img src="${link}">
 	    <figcaption>${name}</figcaption>
       <button onClick="deletarImagem(this)">Excluir</button>
     </figure>
     `
+    idFilme++
   }
+
+  /*
+  Caso a variável seja falsa, ele adiciona o filme no array e no html
+  */
   
 }
 
 function deletarImagem($this) {
-  var element = $this
-  console.log(element.parent)
+  var filme = $this.parentElement
+  listaFilmes.splice(listaFilmes.indexOf(parseInt(filme.className)), 1)
+  filme.remove()
 }
+
+/*
+Ao clicar no botão ele pega o elemento pai 'figure' e remove do array de imagens
+O controle é feito pelo nnome da classe, atribuida dinamicamente, presente no figure
+Em seguida exclui o HTML
+*/
